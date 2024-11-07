03637d731bc29e21eb5306fb72b299c1729f99de
Arham097 <arhamhasan70@gmail.com>
2024-11-08 01:29:09 +0500

    menu page added

--- client/src/Pages/Menu/Menu.jsx
+++ client/src/Pages/Menu/Menu.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useRef } from "react";
 
 const Menu = () => {
-  return <div></div>;
+  const menu = useRef(null);
+  useGSAP(() => {
+    gsap.from(menu.current, {
+      y: 450,
+      duration: 1.5,
+      ease: "power2.out",
+      opacity: 0,
+    });
+  });
+  return (
+    <div className="bg-slate-800 w-screen min-h-screen flex justify-center">
+      <div className="w-1/2 h-full" ref={menu}>
+        <img src="./Menu/Menu.png" alt="" className="w-full h-full" />
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
